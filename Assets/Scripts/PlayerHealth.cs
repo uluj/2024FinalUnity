9b871f3ed5b4b047e4e2cfb5a2314b712b94530f
uluj <gungoruluc@gmail.com>
2025-01-12 15:53:37 +0300

    healthbar

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,23 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class PlayerHealth : MonoBehaviour
 {
-    public int _playerHealth=100;
-
+    public int playerHealth=100;
+    public Slider slider;
     public void HealthCheck()
     {
-        if (_playerHealth > 0)
+        if (playerHealth > 0)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
     public void DecreaseHealth(int damage)
     {
-        _playerHealth -= damage;
-        HealthCheck();
+        playerHealth -= damage;
+        slider.value = playerHealth; // Update the slider
+        Invoke(nameof(HealthCheck), 0.5f);
     }
 
 }
